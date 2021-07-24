126c941fe86d8c2956f6d0a7cfe43bb44480a31f
hieuhv <hieuhv@gameestudio.com>
2021-07-24 08:00:45 +0700

    Update

---
+++ Assets/_Root/Scripts/Ads/AdsManager.cs
@@ -0,0 +1,15 @@
+﻿using GameName.Pattern;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdsManager : Singleton<AdsManager>
+{
+    // show
+    public void ShowAdsBanner() { }
+    public void ShowAdsInterstitial() { }
+    public void ShowAdsRewared() { }
+
+    // hide
+    public void HideAdsBanner() { }
+}
---
+++ Assets/_Root/Scripts/GamePlay/Gamemanager.cs
@@ -0,0 +1,69 @@
+﻿using GameName.Pattern;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class Gamemanager : Singleton<Gamemanager>
+{
+    public GamePlayController gamePlayController;
+    public Transform levelTransform;
+
+    public EGameStatus eGameStatus;
+
+
+    private GameObject _level;
+
+    private void OnEnable()
+    {
+        if(levelTransform == null) 
+        {
+            GameObject level = new GameObject("_Level");
+            levelTransform = level.transform;
+        }
+    }
+    private void Start()
+    {
+        LoadLevelMap();
+    }
+
+    // level map
+    private void LoadLevelMap() 
+    {
+        Addressables.LoadAssetAsync<GameObject>(string.Format("Level_{0}", GameData.LevelCurrent));
+
+        _level.SetActive(false);
+    }
+    private void ShowLevelMap() 
+    {
+        eGameStatus = EGameStatus.GAME_PLAYING;
+        _level.SetActive(true);
+    }
+    private void DestroyLevelMap() { Destroy(_level); }
+
+    // win - lose
+    public void GameWin() 
+    {
+        GameData.LevelCurrent++;
+        gamePlayController.ShowWin();
+        LoadLevelMap();
+    }
+    public void GameLose() 
+    {
+        gamePlayController.ShowLose();
+    }
+
+    // ads 
+    private void ShowAdsBanner() { AdsManager.Instance.ShowAdsBanner(); }
+    private void ShowAdsInterstitial() { AdsManager.Instance.ShowAdsInterstitial(); }
+    private void HideBanner() { AdsManager.Instance.HideAdsBanner(); }
+
+    public enum EGameStatus
+    {
+        GAME_START,
+        GAME_PRE_PLAYING,
+        GAME_PLAYING,
+        GAME_WIN,
+        GAME_LOSE
+    }
+}
